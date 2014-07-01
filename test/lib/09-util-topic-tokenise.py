cabcada8496e7e3db94edd08ea262c7e31feee38
Roger A. Light <roger@atchoo.org>
2014-07-01 22:33:03 +0100

    Test fixes.

--- test/lib/09-util-topic-tokenise.py
+++ test/lib/09-util-topic-tokenise.py
@@ -10,6 +10,7 @@ import time
 cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"..")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
+import mosq_test
 
 rc = 1
 
