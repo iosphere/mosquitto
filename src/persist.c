facb15823f4c2125f18dc278d72a9bac9eaea0d0
Roger A. Light <roger@atchoo.org>
2014-10-08 20:51:17 +0100

    Fix saving persistent database on Windows.
    Thanks to Daniel Degasperi.

--- src/persist.c
+++ src/persist.c
@@ -402,6 +402,12 @@ int mqtt3_db_backup(struct mosquitto_db *db, bool cleanup, bool shutdown)
 
 	fclose(db_fptr);
 
+#ifdef WIN32
+	if(remove(db->config->persistence_filepath) != 0){
+		goto error;
+	}
+}
+#endif
 	if(rename(outfile, db->config->persistence_filepath) != 0){
 		goto error;
 	}
