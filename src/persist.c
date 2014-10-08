0dcf9170b25868d12f7854f55e2d281b30a45bba
Roger A. Light <roger@atchoo.org>
2014-10-08 22:48:47 +0100

    Typo fix.

--- src/persist.c
+++ src/persist.c
@@ -406,7 +406,6 @@ int mqtt3_db_backup(struct mosquitto_db *db, bool cleanup, bool shutdown)
 	if(remove(db->config->persistence_filepath) != 0){
 		goto error;
 	}
-}
 #endif
 	if(rename(outfile, db->config->persistence_filepath) != 0){
 		goto error;
