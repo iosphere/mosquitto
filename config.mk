3cab5e2e69f3d650f9d8e261be42ac906359ed83
Roger A. Light <roger@atchoo.org>
2015-11-08 21:25:04 +0000

    Bump version number.

--- config.mk
+++ config.mk
@@ -83,7 +83,7 @@ WITH_SOCKS:=yes
 
 # Also bump lib/mosquitto.h, CMakeLists.txt,
 # installer/mosquitto.nsi, installer/mosquitto-cygwin.nsi
-VERSION=1.4.4
+VERSION=1.4.5
 TIMESTAMP:=$(shell date "+%F %T%z")
 
 # Client library SO version. Bump if incompatible API/ABI changes are made.
