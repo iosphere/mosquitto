f58f8aac0f9ef23cc6999e52d1e1ba9cdf8adeaa
Roger A. Light <roger@atchoo.org>
2015-11-16 16:47:12 +0000

    Document updates.

--- lib/mosquitto.h
+++ lib/mosquitto.h
@@ -576,6 +576,7 @@ libmosq_EXPORT int mosquitto_disconnect(struct mosquitto *mosq);
  *               Note that although the MQTT protocol doesn't use message ids
  *               for messages with QoS=0, libmosquitto assigns them message ids
  *               so they can be tracked with this parameter.
+ *  topic -      null terminated string of the topic to publish to.
  * 	payloadlen - the size of the payload (bytes). Valid values are between 0 and
  *               268,435,455.
  * 	payload -    pointer to the data to send. If payloadlen > 0 this must be a
